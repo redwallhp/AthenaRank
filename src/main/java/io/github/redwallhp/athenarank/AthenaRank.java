cf4a686a98f35d4a1e9fde4cace1bf04d9ed1887
redwallhp <mcredwallhp@gmail.com>
2016-08-03 01:36:09 -0400

    Implementing head wall, fixed initial table creation to use IF NOT EXISTS

--- src/main/java/io/github/redwallhp/athenarank/AthenaRank.java
+++ src/main/java/io/github/redwallhp/athenarank/AthenaRank.java
@@ -1,6 +1,8 @@
 package io.github.redwallhp.athenarank;
 
+import io.github.redwallhp.athenagm.AthenaGM;
 import io.github.redwallhp.athenarank.leaderboard.Leaderboard;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.Connection;
@@ -14,6 +16,7 @@ public class AthenaRank extends JavaPlugin {
     public static AthenaRank instance;
     private MySQLPool pool;
     private Leaderboard leaderboard;
+    private AthenaGM athena;
 
 
     @Override
@@ -22,6 +25,7 @@ public class AthenaRank extends JavaPlugin {
         saveDefaultConfig();
         pool = new MySQLPool(getConfig());
         createTables();
+        checkAthena();
         new RankEventListener();
         leaderboard = new Leaderboard();
         new CommandHandler();
@@ -36,17 +40,24 @@ public class AthenaRank extends JavaPlugin {
     }
 
 
+    /**
+     * Get a connection from the MySQL pool
+     * @throws SQLException
+     */
     public Connection getSQLConnection() throws SQLException {
         return pool.getConnection();
     }
 
 
+    /**
+     * Initialize the database tables
+     */
     private void createTables() {
         try {
             Connection conn = getSQLConnection();
             if (conn == null) return;
             Statement stmt = conn.createStatement();
-            String sql = "CREATE TABLE `rankings` (" +
+            String sql = "CREATE TABLE IF NOT EXISTS `rankings` (" +
                     "`uuid` VARCHAR(36) NOT NULL," +
                     "`name` VARCHAR(16)," +
                     "`kills` INTEGER NOT NULL DEFAULT '0'," +
@@ -56,15 +67,43 @@ public class AthenaRank extends JavaPlugin {
                     ");";
             stmt.executeUpdate(sql);
             conn.close();
-            getLogger().info("Created initial database tables.");
         } catch (SQLException ex) {
             getLogger().warning("Error creating database tables: " + ex.getMessage());
         }
     }
 
 
+    /**
+     * Returns the Leaderboard handler
+     */
     public Leaderboard getLeaderboard() {
         return leaderboard;
+    }
+
+
+    /**
+     * Load the server's AthenaGM instance, returning false if AthenaGM is not installed.
+     * @return true if AthenaGM is installed and active, false otherwise
+     */
+    private boolean checkAthena() {
+        Plugin plugin = getServer().getPluginManager().getPlugin("AthenaGM");
+        if (plugin == null || !(plugin instanceof AthenaGM)) {
+            getLogger().warning("AthenaGM is not present. Disabling.");
+            this.setEnabled(false);
+            return false;
+        } else {
+            athena = (AthenaGM) plugin;
+            return true;
+        }
+    }
+
+
+    /**
+     * Get the server's AthenaGM instance
+     * @return AthenaGM instance
+     */
+    public AthenaGM getAthena() {
+        return athena;
     }
 
 
--- src/main/java/io/github/redwallhp/athenarank/leaderboard/Leaderboard.java
+++ src/main/java/io/github/redwallhp/athenarank/leaderboard/Leaderboard.java
@@ -1,11 +1,21 @@
 package io.github.redwallhp.athenarank.leaderboard;
 
 import io.github.redwallhp.athenarank.AthenaRank;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.SkullType;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.Sign;
+import org.bukkit.block.Skull;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -31,9 +41,18 @@ public class Leaderboard {
             }
         }.runTaskTimerAsynchronously(plugin, 0L, 1200L);
 
+        new BukkitRunnable() {
+            public void run() {
+                buildHubLeaderboard();
+            }
+        }.runTaskTimer(plugin, 20L, 1220L);
+
     }
 
 
+    /**
+     * Load the top players from the database
+     */
     private void loadTopEntries() {
         try {
             Connection conn = plugin.getSQLConnection();
@@ -61,8 +80,108 @@ public class Leaderboard {
     }
 
 
+    /**
+     * Return the cached top players list
+     */
     public List<LeaderboardEntry> getTopEntries() {
         return topEntries;
+    }
+
+
+    /**
+     * Convert a "0,0,0" (x,y,z) string into a vector object
+     */
+    private Vector parseVectorString(String vectorString) {
+        vectorString = vectorString.replaceAll("\\s+",""); //strip spaces
+        String[] components = vectorString.split(",");
+        try {
+            double x = Double.parseDouble(components[0]);
+            double y = Double.parseDouble(components[1]);
+            double z = Double.parseDouble(components[2]);
+            return new Vector(x, y, z);
+        } catch(NumberFormatException ex) {
+            return null;
+        }
+    }
+
+
+    /**
+     * Add leaderboard heads/signs to the Hub
+     */
+    private void buildHubLeaderboard() {
+
+        // Return if this feature, or the Hub, is disabled
+        if (!plugin.getConfig().getBoolean("leaderboard.enabled", false)) return;
+        if (plugin.getAthena().getHub().getWorld() == null) return;
+
+        // Load the coordinates
+        List<Vector> headVectors = new ArrayList<Vector>();
+        List<Vector> signVectors = new ArrayList<Vector>();
+        for (String vs : plugin.getConfig().getStringList("leaderboard.heads")) {
+            Vector vec = parseVectorString(vs);
+            if (vec != null) {
+                headVectors.add(vec);
+            }
+        }
+        for (String vs : plugin.getConfig().getStringList("leaderboard.signs")) {
+            Vector vec = parseVectorString(vs);
+            if (vec != null) {
+                signVectors.add(vec);
+            }
+        }
+
+        // Set the heads
+        World world = plugin.getAthena().getHub().getWorld();
+        for (int i=0; i<headVectors.size(); i++) {
+            Vector vec = headVectors.get(i);
+            LeaderboardEntry entry = getEntryAtIndex(i);
+            Block headBlock = world.getBlockAt(vec.getBlockX(), vec.getBlockY(), vec.getBlockZ());
+            if (headBlock.getType().equals(Material.SKULL)) {
+                Skull skull = (Skull) headBlock.getState();
+                BlockFace facing = skull.getRotation();
+                headBlock.setData((byte) SkullType.PLAYER.ordinal());
+                skull.setOwningPlayer(plugin.getServer().getOfflinePlayer(entry.getUuid()));
+                skull.setRotation(facing);
+                skull.update();
+            }
+        }
+
+        // Set the signs
+        for (int i=0; i<signVectors.size(); i++) {
+            Vector vec = signVectors.get(i);
+            LeaderboardEntry entry = getEntryAtIndex(i);
+            Block signBlock = world.getBlockAt(vec.getBlockX(), vec.getBlockY(), vec.getBlockZ());
+            int rank = i + 1;
+            float kdr = calculateKDR(entry);
+            if (signBlock.getType().equals(Material.WALL_SIGN)) {
+                Sign sign = (Sign) signBlock.getState();
+                sign.setLine(1, String.format("%s%s", ChatColor.BOLD, entry.getName()));
+                sign.setLine(2, String.format("Rank: #%d  KDR: %.1f", rank, kdr));
+                sign.update();
+            }
+        }
+
+    }
+
+
+    private LeaderboardEntry getEntryAtIndex(int i) {
+        if (i < topEntries.size()) {
+            return topEntries.get(i);
+        } else {
+            //MHF_Steve
+            return new LeaderboardEntry(UUID.fromString("c06f8906-4c8a-4911-9c29-ea1dbd1aab82"), "Nobody", 0, 0, 0, 0);
+        }
+    }
+
+
+    private float calculateKDR(LeaderboardEntry entry) {
+        float kdr;
+        try {
+            kdr = entry.getKills() / entry.getDeaths();
+        } catch (ArithmeticException ex) {
+            kdr = entry.getKills();
+        }
+        return kdr;
     }
 
 
